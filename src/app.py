0f8f9465083582408dca3c193612959273bc78bf
sepeca15 <sepeca15@gmail.com>
2022-09-02 21:38:13 +0000

    Kmeans ok + app + models

--- src/app.py
+++ src/app.py
@@ -1,4 +1,23 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+import pickle
 
-# your code here
+url = 'https://raw.githubusercontent.com/4GeeksAcademy/k-means-project-tutorial/main/housing.csv'
+
+df_raw = pd.read_csv(url)
+
+df = df_raw[['Latitude', 'Longitude', 'MedInc']]
+
+df_scaled = StandardScaler().fit_transform(df)
+
+kmeans = KMeans(n_clusters=2)
+
+df["Cluster"] = kmeans.fit_predict(df_scaled)
+df["Cluster"] = df["Cluster"].astype("category")
+
+df.to_csv('../data/processed/clusters.csv')
+
+filename = '../models/housting.pickle'
+pickle.dump(kmeans, open(filename,'wb'))
